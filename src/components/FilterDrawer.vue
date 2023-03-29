4ca888ebec3a5b9b581ed2afcab1cceaf78b77f9
florianfkaemmerer <florian.f.kaemmerer@deutschebahn.com>
2023-03-29 19:54:09 +0200

    fixed mdi icons search by text (WiP)

--- src/components/FilterDrawer.vue
+++ src/components/FilterDrawer.vue
@@ -3,12 +3,29 @@
     <v-label>Search:</v-label>
     <v-responsive>
       <v-text-field
-          clearable
-          label="Search by Text"></v-text-field>
+          v-model="searchText"
+          density="compact"
+          variant="underlined"
+          label="Search... (eg: Atoll, Doctor)"
+          prepend-inner-icon="mdi-magnify"
+          single-line
+          class="mx-2"
+          color="#198754"
+          :hide-details="true"
+          clearable></v-text-field>
     </v-responsive>
   </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
+
+import {ref, watch} from "vue";
+
+let searchText = ref("");
+
+watch( searchText,
+    (searchText) => {
+      console.log(searchText)
+    });
 
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import "@mdi/font/css/materialdesignicons.css";
 import './assets/main.css'
 
 import {createApp} from 'vue'
