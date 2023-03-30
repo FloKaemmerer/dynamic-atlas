38f357947b2084c499543af0fa426461db81aaab
florianfkaemmerer <florian.f.kaemmerer@deutschebahn.com>
2023-03-30 21:54:47 +0200

    added vue-konva

--- src/composable/text-search.ts
+++ src/composable/text-search.ts
@@ -3,6 +3,6 @@ import {useAtlasNodeStore} from "@/store/AtlasNodeStore";
 
 export const handleTextSearch = (searchText: string) => {
     const atlasNodeStore = useAtlasNodeStore();
-    const result = atlasNodeStore.atlasNodes.filter(atlasNode => atlasNode.Name.includes(searchText))
+    const result = atlasNodeStore.atlasNodes.filter(atlasNode => atlasNode.Name.toLowerCase().includes(searchText.toLowerCase()))
     atlasNodeStore.SET_FILTERED_ATLAS_NODE_IDS(result)
 }
--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,16 @@ import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import VueKonva from 'vue-konva';
+
 const vuetify = createVuetify({
     components,
     directives,
 })
-
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(vuetify);
 app.use(router)
+app.use(VueKonva, { prefix: 'k'});
 
 app.mount('#app')
