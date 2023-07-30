125815c9764ef607cabcbbb6587404425c75437b
FloKaemmerer <flo.kaemmerer@gmail.com>
2023-07-30 10:46:58 +0200

    linter

--- src/store/FilterQueryStore/index.ts
+++ src/store/FilterQueryStore/index.ts
@@ -1,17 +1,17 @@
-import {defineStore} from "pinia";
-import type {LooseFilters} from "@/model/looseFilters";
+import { defineStore } from 'pinia'
+import type { LooseFilters } from '@/model/looseFilters'
 
 interface State {
-    filterQuery: LooseFilters;
+  filterQuery: LooseFilters
 }
 
-export const useFilterQueryStore = defineStore("filter-query", {
-    state: (): State => ({
-        filterQuery: {},
-    }),
-    actions: {
-        SET_FILTER_QUERY(filterQuery: LooseFilters) {
-            this.filterQuery = filterQuery
-        },
-    }
-});
+export const useFilterQueryStore = defineStore('filter-query', {
+  state: (): State => ({
+    filterQuery: {},
+  }),
+  actions: {
+    SET_FILTER_QUERY(filterQuery: LooseFilters) {
+      this.filterQuery = filterQuery
+    },
+  },
+})
