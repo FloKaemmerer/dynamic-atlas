2023ef5ec925eed445f1cf8dace7c09343af87ee
FloKaemmerer <flo.kaemmerer@gmail.com>
2023-08-01 17:59:03 +0200

    Linter

--- src/components/filter_drawer/filters/boss_filters/SkippablePhasesFilter.vue
+++ src/components/filter_drawer/filters/boss_filters/SkippablePhasesFilter.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useFilterStore } from '@/store/FilterStore'
 
-import {useFilterStore} from "@/store/FilterStore";
-import {ref} from "vue";
-
-const filterStore = useFilterStore();
-let includeSkippablePhases = ref(filterStore.includeSkippablePhases)
+const filterStore = useFilterStore()
+const includeSkippablePhases = ref(filterStore.includeSkippablePhases)
 
 filterStore.$subscribe((mutation, state) => {
-    if (state.includeSkippablePhases != includeSkippablePhases.value) {
-        includeSkippablePhases.value = state.includeSkippablePhases
-    }
+  if (state.includeSkippablePhases !== includeSkippablePhases.value) {
+    includeSkippablePhases.value = state.includeSkippablePhases
+  }
 })
 
 function handleIncludeSkippablePhasesFilter() {
-    filterStore.SET_INCLUDE_SKIPPABLE_PHASES(!includeSkippablePhases.value)
+  filterStore.SET_INCLUDE_SKIPPABLE_PHASES(!includeSkippablePhases.value)
 }
-
 </script>
 
 <template>
-    <v-checkbox label="Include Bosses With skippable Phases"
-                v-model="includeSkippablePhases" density="compact"
-                @click="handleIncludeSkippablePhasesFilter()"
-                :disabled="!filterStore.excludePhasedBosses"></v-checkbox>
+  <v-checkbox
+    v-model="includeSkippablePhases"
+    label="Include Bosses With skippable Phases" density="compact"
+    :disabled="!filterStore.GET_SELECTED_FILTER().excludePhasedBosses"
+    @click="handleIncludeSkippablePhasesFilter()"
+  />
 </template>
 
 <style scoped>
