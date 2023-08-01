188cbbe54f8f91368caa8e154bfadb0b59c6b929
FloKaemmerer <flo.kaemmerer@gmail.com>
2023-08-01 10:41:15 +0200

    linter

--- src/components/filter_drawer/filters/map_filters/RushableBossFilter.vue
+++ src/components/filter_drawer/filters/map_filters/RushableBossFilter.vue
@@ -1,39 +1,44 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {useFilterStore} from "@/store/FilterStore";
+import { ref } from 'vue'
+import { useFilterStore } from '@/store/FilterStore'
 
-const filterStore = useFilterStore();
+const filterStore = useFilterStore()
 
-let rushableBoss = ref(filterStore.rushableBoss)
+const rushableBoss = ref(filterStore.rushableBoss)
 
 filterStore.$subscribe((mutation, state) => {
-    if (state.rushableBoss != rushableBoss.value) {
-        rushableBoss.value = state.rushableBoss
-    }
+  if (state.rushableBoss !== rushableBoss.value) {
+    rushableBoss.value = state.rushableBoss
+  }
 })
 
 function handleRushableBossFilter() {
-    filterStore.SET_RUSHABLE_BOSS(!rushableBoss.value)
+  filterStore.SET_RUSHABLE_BOSS(!rushableBoss.value)
 }
 </script>
 
 <template>
-    <v-row no-gutters>
-        <v-checkbox label="Rushable Boss"
-                    v-model="rushableBoss"
-                    @click="handleRushableBossFilter()"
-                    density="compact">
-            <template v-slot:append>
-                <v-tooltip
-                        text="A Boss is considered Rushable, if it is easy and/or predictable to reach (eg. Mesa, Dunes)">
-                    <template v-slot:activator="{ props }">
-                        <v-icon icon="mdi-information-outline"
-                                v-bind="props"></v-icon>
-                    </template>
-                </v-tooltip>
-            </template>
-        </v-checkbox>
-    </v-row>
+  <v-row no-gutters>
+    <v-checkbox
+      v-model="rushableBoss"
+      label="Rushable Boss"
+      density="compact"
+      @click="handleRushableBossFilter()"
+    >
+      <template #append>
+        <v-tooltip
+          text="A Boss is considered Rushable, if it is easy and/or predictable to reach (eg. Mesa, Dunes)"
+        >
+          <template #activator="{ props }">
+            <v-icon
+              icon="mdi-information-outline"
+              v-bind="props"
+            />
+          </template>
+        </v-tooltip>
+      </template>
+    </v-checkbox>
+  </v-row>
 </template>
 
 <style scoped>
