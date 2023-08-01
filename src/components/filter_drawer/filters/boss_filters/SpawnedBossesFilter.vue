7db3de57510794d1a927eec6f6c07367f84ec92c
FloKaemmerer <flo.kaemmerer@gmail.com>
2023-08-01 18:22:02 +0200

    Linter

--- src/components/filter_drawer/filters/boss_filters/SpawnedBossesFilter.vue
+++ src/components/filter_drawer/filters/boss_filters/SpawnedBossesFilter.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useFilterStore } from '@/store/FilterStore'
 
-import {useFilterStore} from "@/store/FilterStore";
-import {ref} from "vue";
-
-const filterStore = useFilterStore();
-let excludeSpawnedBosses = ref(filterStore.excludeSpawnedBosses)
+const filterStore = useFilterStore()
+const excludeSpawnedBosses = ref(filterStore.excludeSpawnedBosses)
 
 filterStore.$subscribe((mutation, state) => {
-    if (state.excludeSpawnedBosses != excludeSpawnedBosses.value) {
-        excludeSpawnedBosses.value = state.excludeSpawnedBosses
-    }
+  if (state.excludeSpawnedBosses !== excludeSpawnedBosses.value) {
+    excludeSpawnedBosses.value = state.excludeSpawnedBosses
+  }
 })
 
 function handleExcludeSpawnedBossesFilter() {
-    filterStore.SET_EXCLUDE_SPAWNED_BOSSES(!excludeSpawnedBosses.value)
+  filterStore.SET_EXCLUDE_SPAWNED_BOSSES(!excludeSpawnedBosses.value)
 }
 </script>
 
 <template>
-    <v-checkbox label="Exclude Spawned Bosses"
-                v-model="excludeSpawnedBosses"
-                @click="handleExcludeSpawnedBossesFilter()"
-                density="compact"></v-checkbox>
+  <v-checkbox
+    v-model="excludeSpawnedBosses"
+    label="Exclude Spawned Bosses"
+    density="compact"
+    @click="handleExcludeSpawnedBossesFilter()"
+  />
 </template>
 
 <style scoped>
