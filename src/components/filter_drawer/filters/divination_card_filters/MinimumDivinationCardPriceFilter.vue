3a864e7a79ef161f921f6b293f6b4f73f0899c95
FloKaemmerer <flo.kaemmerer@gmail.com>
2023-08-01 18:25:30 +0200

    Linter

--- src/components/filter_drawer/filters/divination_card_filters/MinimumDivinationCardPriceFilter.vue
+++ src/components/filter_drawer/filters/divination_card_filters/MinimumDivinationCardPriceFilter.vue
@@ -1,46 +1,46 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useFilterStore } from '@/store/FilterStore'
 
-import {ref} from "vue";
-import {useFilterStore} from "@/store/FilterStore";
-
-const filterStore = useFilterStore();
-let minDivinationCardPrice = ref(filterStore.minDivinationCardPrice)
+const filterStore = useFilterStore()
+const minDivinationCardPrice = ref(filterStore.minDivinationCardPrice)
 
 filterStore.$subscribe((mutation, state) => {
-    if (state.minDivinationCardPrice != minDivinationCardPrice.value) {
-        minDivinationCardPrice.value = state.minDivinationCardPrice
-    }
+  if (state.minDivinationCardPrice !== minDivinationCardPrice.value) {
+    minDivinationCardPrice.value = state.minDivinationCardPrice
+  }
 })
 
 function handleMinimumDivinationCardPriceFilter(value: string) {
-    filterStore.SET_MINIMUM_DIVINATION_CARD_PRICE(Number.parseInt(value))
+  filterStore.SET_MINIMUM_DIVINATION_CARD_PRICE(Number.parseInt(value))
 }
 </script>
 
 <template>
-    <v-label>
-        Minimal Price in Chaos Orbs
-    </v-label>
-    <v-text-field
-            v-model="minDivinationCardPrice"
-            @update:model-value="val => handleMinimumDivinationCardPriceFilter(val)"
-            hide-details
-            single-line
-            label="Minimal Price in Chaos Orbs"
-            type="number"
-            variant="outlined"
-            style="width: 100%"
-            density="compact"
-            min="0">
-        <template v-slot:append>
-            <v-tooltip>
-                <template v-slot:activator="{ props }">
-                    <v-icon icon="mdi-information-outline" v-bind="props"></v-icon>
-                </template>
-                <p>Filter based on the minimal required estimated price of a Divination Card that drops in a Map.</p>
-            </v-tooltip>
+  <v-label>
+    Minimal Price in Chaos Orbs
+  </v-label>
+  <v-text-field
+    v-model="minDivinationCardPrice"
+    hide-details
+    single-line
+    label="Minimal Price in Chaos Orbs"
+    type="number"
+    variant="outlined"
+    style="width: 100%"
+    density="compact"
+    min="0"
+    @update:model-value="val => handleMinimumDivinationCardPriceFilter(val)"
+  >
+    <template #append>
+      <v-tooltip>
+        <template #activator="{ props }">
+          <v-icon icon="mdi-information-outline" v-bind="props" />
         </template>
-    </v-text-field>
+        <p>Filter based on the minimal required estimated price of a Divination Card that drops in a Map.</p>
+      </v-tooltip>
+    </template>
+  </v-text-field>
 </template>
 
 <style scoped>
