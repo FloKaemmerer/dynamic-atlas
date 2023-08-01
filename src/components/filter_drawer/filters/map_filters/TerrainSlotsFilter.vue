ba9cacaa24a3037d8c3feaebd23786914fbd0804
FloKaemmerer <flo.kaemmerer@gmail.com>
2023-08-01 16:02:22 +0200

    Linter

--- src/components/filter_drawer/filters/map_filters/TerrainSlotsFilter.vue
+++ src/components/filter_drawer/filters/map_filters/TerrainSlotsFilter.vue
@@ -1,74 +1,76 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import {useFilterStore} from "@/store/FilterStore";
+import { ref } from 'vue'
+import { useFilterStore } from '@/store/FilterStore'
 
-const filterStore = useFilterStore();
+const filterStore = useFilterStore()
 
-let includeTerrainSlots = ref(filterStore.includeTerrainSlots)
-let terrainSlots = ref(filterStore.terrainSlots)
+const includeTerrainSlots = ref(filterStore.includeTerrainSlots)
+const terrainSlots = ref(filterStore.terrainSlots)
 
 filterStore.$subscribe((mutation, state) => {
-    if (state.includeTerrainSlots != includeTerrainSlots.value) {
-        includeTerrainSlots.value = state.includeTerrainSlots
-    }
-    if (state.terrainSlots != terrainSlots.value) {
-        terrainSlots.value = state.terrainSlots
-    }
+  if (state.includeTerrainSlots !== includeTerrainSlots.value) {
+    includeTerrainSlots.value = state.includeTerrainSlots
+  }
+  if (state.terrainSlots !== terrainSlots.value) {
+    terrainSlots.value = state.terrainSlots
+  }
 })
 
 function handleIncludeTerrainSlotsFilter() {
-    filterStore.SET_INCLUDE_TERRAIN_SLOTS(!includeTerrainSlots.value)
+  filterStore.SET_INCLUDE_TERRAIN_SLOTS(!includeTerrainSlots.value)
 }
 
 function debounceTerrainSlotsFilter(value: [number, number]) {
-    if (!(value[0] == filterStore.terrainSlots[0] && value[1] == filterStore.terrainSlots[1])) {
-        filterStore.SET_TERRAIN_SLOTS(value)
-    }
+  if (!(value[0] === filterStore.terrainSlots[0] && value[1] === filterStore.terrainSlots[1])) {
+    filterStore.SET_TERRAIN_SLOTS(value)
+  }
 }
 </script>
 
 <template>
-    <v-row no-gutters>
-        <v-col>
-            Terrain Slots
-            <v-tooltip
-                    text="The number of slots within the map terrain, which can spawn a League Mechanic, aka the juiceability of a Map.">
-                <template v-slot:activator="{ props }">
-                    <v-icon icon="mdi-information-outline" v-bind="props"></v-icon>
-                </template>
-            </v-tooltip>
-        </v-col>
-    </v-row>
-    <v-row no-gutters>
-        <v-col cols="1">
-            <v-checkbox v-model="includeTerrainSlots"
-                        @click="handleIncludeTerrainSlotsFilter()"
-                        density="compact"
-            ></v-checkbox>
-        </v-col>
-        <v-col>
-            <v-range-slider
-                    v-model="terrainSlots"
-                    @update:model-value="value => debounceTerrainSlotsFilter(value)"
-                    strict
-                    direction="horizontal"
-                    step="1"
-                    show-ticks="always"
-                    tick-size="4"
-                    thumb-label
-                    :max="10"
-                    :min="0"
-                    :disabled="!includeTerrainSlots"
-            >
-                <template v-slot:prepend>
-                    Low
-                </template>
-                <template v-slot:append>
-                    High
-                </template>
-            </v-range-slider>
-        </v-col>
-    </v-row>
+  <v-row no-gutters>
+    <v-col>
+      Terrain Slots
+      <v-tooltip
+        text="The number of slots within the map terrain, which can spawn a League Mechanic, aka the juiceability of a Map."
+      >
+        <template #activator="{ props }">
+          <v-icon icon="mdi-information-outline" v-bind="props" />
+        </template>
+      </v-tooltip>
+    </v-col>
+  </v-row>
+  <v-row no-gutters>
+    <v-col cols="1">
+      <v-checkbox
+        v-model="includeTerrainSlots"
+        density="compact"
+        @click="handleIncludeTerrainSlotsFilter()"
+      />
+    </v-col>
+    <v-col>
+      <v-range-slider
+        v-model="terrainSlots"
+        :strict="true"
+        direction="horizontal"
+        step="1"
+        show-ticks="always"
+        tick-size="4"
+        thumb-label
+        :max="10"
+        :min="0"
+        :disabled="!includeTerrainSlots"
+        @update:model-value="value => debounceTerrainSlotsFilter(value)"
+      >
+        <template #prepend>
+          Low
+        </template>
+        <template #append>
+          High
+        </template>
+      </v-range-slider>
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped>
